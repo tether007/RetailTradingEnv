12b4e42c204b9dc5d9b82f978bac7a32a2302801
tether007 <toshrosh2005@gmail.com>
2026-04-12 23:30:40 +0530

    fix openenv-core dep

--- trade_env/server/__init__.py
+++ trade_env/server/__init__.py
@@ -4,8 +4,5 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""Trade Env environment server components."""
 
-from .environment import TradeEnvironment
-
-__all__ = ["TradeEnvironment"]
+__all__ = []
